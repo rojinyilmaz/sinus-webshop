27b47294a5bc6bfc0e458724d35a95351f4ca916
rojinyilmaz <70367334+rojinyilmaz@users.noreply.github.com>
2021-02-23 20:47:53 +0100

    Updated checkout page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
   }
 ]
 
---
+++ frontend/src/views/Checkout.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="wrapper">
+  <main class="checkout">
+<hr class="line1"/>
+      <h3>My cart</h3>
+  </main>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.wrapper{
+    background-color: black;
+     height: 89vh;
+    margin-left: -1%;
+  margin-right: -1%;
+  margin-bottom: -1%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-position: right, left;
+}
+
+.checkout{
+    background-color: #ffff;
+    display: flex;
+        flex-direction: column;
+        text-align: left;
+        width: 96%;
+        height: 84vh;
+        border-radius: 3px;
+}
+
+.line1{
+    border: 2px solid black;
+    width: 90%;
+    margin-top: 5rem;
+}
+
+
+h3{
+    text-decoration: underline;
+    margin-left: 3em;
+}
+</style>
