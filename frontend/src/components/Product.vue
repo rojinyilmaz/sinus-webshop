3c455289e0352f1ab85794e260604f37624b3797
flure1 <flure@Tim.localdomain>
2021-02-24 12:04:13 +0100

    vue files

---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+<section>
+    <div v-for ="c in test" :key=c.id class="productSection">
+        <div class="pwrapper">
+            <h3>{{c.name}}</h3>
+            <p>{{c.desc}}</p>
+            <h4>{{c.price}} €</h4>
+        </div>
+    </div>
+</section>
+  
+</template>
+
+<script>
+export default {
+
+computed:{
+    test()
+    {
+        return this.$store.getters.clothes;
+    }
+}
+
+}
+
+</script>
+<style>
+
+.productSection{
+    display: grid;
+    justify-content: center;
+    align-content: space-between;
+}
+
+</style>
---
+++ frontend/src/views/ProductPage.vue
@@ -0,0 +1,20 @@
+<template>
+
+<section class="productwrapper">
+    <Product/>
+
+</section>
+  
+</template>
+
+<script>
+import Product from '@/components/Product.vue'
+export default {
+    components: { Product }
+
+}
+</script>
+
+<style>
+
+</style>
