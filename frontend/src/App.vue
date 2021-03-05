094aa31fd85fbb3a7383b51399f60f1df7115863
rojinyilmaz <70367334+rojinyilmaz@users.noreply.github.com>
2021-03-05 12:32:12 +0100

    Checkout delivery adress form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/productpage">Products</router-link> |
+      <router-link to="/checkout">Checkout</router-link> |
       <router-link to="/register">Register</router-link> |
       <router-link to="/myaccount">Log in / My account</router-link> 
     </div>
